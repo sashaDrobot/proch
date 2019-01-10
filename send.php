413609175a9b3109c27715a1cf67222e39adf750
sashaDrobot <alex.drobot22@gmail.com>
2019-01-10 23:59:46 +0200

    v 0.0.3 gallery

--- send.php
+++ send.php
@@ -4,7 +4,7 @@ $name = $_POST['name'] ?? false;
 $mail = $_POST['email'] ?? false;
 $msg = $_POST['message'] ?? false;
 
-$email = "alex.drobot22@gmail.com";
+$email = "dprochuhan@gmail.com";
 $subject = "Новый вопрос";
 $headers = 'Content-type: text/html; charset=UTF-8';
 $message = "Имя: $name<br/>Email: $mail<br/>Сообщение: $msg";
